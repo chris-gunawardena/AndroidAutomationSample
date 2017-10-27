c46f631e824ba13e876f7d8f1fd5d9eab37c57fc
Chris Gunawardena <chris.gun@nl.ibm.com>
2017-10-27 10:32:30 +0200

    Wip

--- app/src/androidTest/java/com/ibm/loginregistration/IntRegistrationLoginTest.java
+++ app/src/androidTest/java/com/ibm/loginregistration/IntRegistrationLoginTest.java
@@ -1,6 +1,7 @@
 package com.ibm.loginregistration;
 
 
+import android.content.Context;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -9,15 +10,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
+import com.github.tomakehurst.wiremock.common.ConsoleNotifier;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Random;
-
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
@@ -28,6 +36,9 @@ import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.any;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
 
@@ -36,7 +47,20 @@ import static org.hamcrest.Matchers.is;
 public class IntRegistrationLoginTest {
 
     @Rule
-    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class, false, false);
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(WireMockConfiguration.wireMockConfig().port(8080)
+            .notifier(new ConsoleNotifier(true)), false);
+
+    @Before
+    public void setup() {
+        wireMockRule.stubFor(any(urlEqualTo("/v2/configuration"))
+                .atPriority(5).willReturn(aResponse()
+                        .withBody(asset("testdata/configuration.json"))));
+        mActivityTestRule.launchActivity(null);
+    }
+
 
     @Test
     public void e2ERegisterLoginTest() {
@@ -181,6 +205,35 @@ public class IntRegistrationLoginTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Returns the content of a file in the path
+     * @param assetPath File path
+     * @return
+     */
+    public static String asset(String assetPath) {
+        Context context = getInstrumentation().getContext();
+        try {
+            InputStream inputStream = context.getAssets().open(assetPath);
+            return inputStreamToString(inputStream, "UTF-8");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    private static final int BUFFER_SIZE = 4 * 1024;
+    private static String inputStreamToString(InputStream inputStream, String charsetName)
+            throws IOException {
+        StringBuilder builder = new StringBuilder();
+        InputStreamReader reader = new InputStreamReader(inputStream, charsetName);
+        char[] buffer = new char[BUFFER_SIZE];
+        int length;
+        while ((length = reader.read(buffer)) != -1) {
+            builder.append(buffer, 0, length);
+        }
+        return builder.toString();
     }
 
 }
